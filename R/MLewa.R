1844bfb6235b5d1a919ae72aa1fedd7aa7d9651c
Pierre Gaillard <pierre@gaillard.me>
2015-09-03 16:42:21 +0200

    remove href and period from ML algos

--- R/MLewa.R
+++ R/MLewa.R
@@ -1,13 +1,14 @@
 
 MLewa <-
 function(y, experts, awake = NULL, 
-                  loss.type='squareloss', loss.gradient = TRUE,
-                  period = 1, href = 1)
+                  loss.type='squareloss', loss.gradient = TRUE, w0 = NULL)
 {
    experts <- as.matrix(experts)
    N <- ncol(experts)
    T <- nrow(experts)
    
+   if (is.null(w0)) {w0 <- rep(1,N)} # Uniform intial weight vector if unspecified
+
    # Experts are always active if awake is unspecified
    if (is.null(awake)) {awake = matrix(1, nrow = T, ncol = N)} 
    awake <- as.matrix(awake)
@@ -15,28 +16,24 @@ function(y, experts, awake = NULL,
    awake[idx.na] <- 0
    experts[idx.na] <- 0
 
-   R <- rep(0,N)
+   R = rep(0,N)     # Regret vector
+   w <- w0
    
    # weight assigned to each expert
    weights <- matrix(0, ncol = N, nrow = T)
    prediction <- rep(0, T)
    
-   w <- rep(1,N)/N
-   wop <- rep(1,N)/N
-   
    # Initialization or the learning parameter
-   etaop <- matrix(exp(350),ncol=N,nrow=T+1)
-   eta <- rep(exp(350),N)
-   
+   eta <- matrix(exp(350),ncol=N,nrow=T+1)
    for(t in 1:T){
-      # form the operational mixture and the prediction of the aggregation rule
-      weights[t,] <- awake[t,] * wop / sum(awake[t,] * wop) 
-      prediction[t] <- experts[t,] %*% weights[t,]
-      
       # form the each-instant updated mixture and prediction
       p <- awake[t,] * w / sum(awake[t,] * w) 
-      pred <- experts[t,] %*% p 
-      
+      pred <- experts[t,] %*% p     
+
+      # form the operational mixture and the prediction of the aggregation rule
+      weights[t,] <- p
+      prediction[t] <- pred
+
       # observe losses
       lpred <- lossPred(pred, y[t], pred, 
                          loss.type = loss.type, loss.gradient = loss.gradient)
@@ -46,17 +43,11 @@ function(y, experts, awake = NULL,
       # update regret and weights
       r <- awake[t,] * (lpred - lexp)
       R <- R + r
-      eta = sqrt(log(N)/(log(N)/eta^2+r^2))
-      w <- truncate1(exp(eta*R)) 
-      
-      # If it is time to update...
-      h <- (((t - 1) %% period) + 1)
-      if (h == href) {
-         wop <- w
-         etaop[t+1,] <- eta
-      } else {
-         etaop[t+1,] <- etaop[t,]
-      }
+      eta[t+1,] = sqrt(log(N)/(log(N)/eta[t,]^2+r^2))
+      w <- truncate1(exp(log(w0) + eta[t+1,]*R)) 
    }
-   return(list(weights = weights, prediction = prediction, eta = etaop))
+   w = w / sum(w)
+
+   return(list(weights = weights, prediction = prediction, eta = eta,
+              weights.forecast =  w))
 }
--- R/MLprod.R
+++ R/MLprod.R
@@ -1,73 +1,60 @@
 MLprod <-
 function(y, experts, awake = NULL, 
-                   loss.type = 'squareloss', loss.gradient = TRUE,
-                   href = 1, period = 1, tau = 0.5)
+             loss.type = 'squareloss', loss.gradient = TRUE, tau = 0.5, w0 = NULL)
 {
-   
-   experts <- as.matrix(experts)
-   N <- ncol(experts)
-   T <- nrow(experts)
-   
-   if (is.null(awake)) {awake = matrix(1, nrow = T, ncol = N)} # Activation 1 si non spécifiée
-   awake = as.matrix(awake)
-   idx.na <- which(is.na(experts))
-   awake[idx.na] <- 0
-   experts[idx.na] <- 0
+  
+  experts <- as.matrix(experts)
+  N <- ncol(experts)
+  T <- nrow(experts)
+  
+  if (is.null(w0)) {w0 <- rep(1,N)} # Uniform intial weight vector if unspecified
+  if (is.null(awake)) {awake = matrix(1, nrow = T, ncol = N)} # Activation 1 si non spécifiée
+  awake = as.matrix(awake)
+  idx.na <- which(is.na(experts))
+  awake[idx.na] <- 0
+  experts[idx.na] <- 0
 
-   weights <- matrix(0,ncol=N,nrow=T)
-   R <- rep(0,N)
-   Rop <- rep(0,N)
-   
-   L <- rep(1, N)
-   maxloss <- 0
-   gamma <- 1/2
-   eta <- rep(exp(700),N)
-   etaop <- matrix(exp(700),ncol=N,nrow=T+1)
-   
-   prediction <- rep(0,T)
-   
-   for(t in 1:T){
-      # Operational update
-      wop <- truncate1(exp(Rop))
-      wop <- etaop[t,] * wop / sum(etaop[t,] * wop)
-      weights[t,] <- awake[t,] * wop  / sum(awake[t,] * wop)
-      prediction[t] <- experts[t,] %*% weights[t,]
-      
-      # Update weights
-      w <- truncate1(exp(R));
-      w <- eta * w / sum(eta * w)
-      p <- awake[t,] * w  / sum(awake[t,] * w) 
-      
-      # Predict
-      pred <- experts[t,] %*% p  
-      
-      # Observe losses
-      lpred <- lossPred(pred,  y[t], pred, tau = tau,
-                         loss.type = loss.type, loss.gradient = loss.gradient)
-      lexp <- lossPred(experts[t,], y[t], pred, tau = tau,
-                        loss.type = loss.type, loss.gradient = loss.gradient)
-      
-      r = awake[t,]*(lpred - lexp)
-      L = L + r^2
-      maxloss = pmax(maxloss,abs(r))
-      neweta <- pmin(1/(2* maxloss),sqrt(log(N)/L))
-      
-      # Update regret and learning parameter
-      R <- neweta/eta * R + log (1 + awake[t,] * neweta * (lpred - lexp))
-      eta <- neweta
-      
-      if (is.na(sum(R)) ) {
-         browser("Nan in R")
-      }
-      
-      # Si c'est l'heure de mise à jour on met à jour weights
-      h <- (((t-1)%%period)+1)
-      if (h == href) {
-         Rop <- R
-         etaop[t+1,] <- eta
-      } else {
-         etaop[t+1,] <- etaop[t,]
-      }
-   }
-   return(list(weights = weights, prediction = prediction, eta = etaop))
+  weights <- matrix(0,ncol=N,nrow=T)
+  R <- log(w0)
+  
+  L <- rep(1, N)
+  maxloss <- 0
+
+  eta <- matrix(exp(700),ncol=N,nrow=T+1)
+  prediction <- rep(0,T)
+  
+  for(t in 1:T){
+    
+    # Update weights
+    w <- truncate1(exp(R))
+    w <- eta[t,] * w / sum(eta[t,] * w)
+    p <- awake[t,] * w  / sum(awake[t,] * w)
+    pred <- experts[t,] %*% p       # Predict
+
+    weights[t,] <- p
+    prediction[t] <- pred
+     
+    # Observe losses
+    lpred <- lossPred(pred,  y[t], pred, tau = tau,
+                 loss.type = loss.type, loss.gradient = loss.gradient)
+    lexp <- lossPred(experts[t,], y[t], pred, tau = tau,
+                loss.type = loss.type, loss.gradient = loss.gradient)
+
+    r = awake[t,] * (lpred - lexp)
+    L = L + r^2
+    maxloss = pmax(maxloss,abs(r))
+    neweta <- pmin(1/(2* maxloss),sqrt(log(N)/L))
+    
+    # Update regret and learning parameter
+    R <- neweta/eta[t,] * R + log (1 + awake[t,] * neweta * (lpred - lexp))
+    eta[t+1,] <- neweta
+    
+    if (is.na(sum(R)) ) {
+      browser("Nan in R")
+    }
+  }
+  w <- truncate1(exp(R))
+  w <- eta[T+1,] * w / sum(eta[T+1,] * w)
+  return(list(weights = weights, prediction = prediction, eta = eta,
+          weights.forecast = w))
 }
