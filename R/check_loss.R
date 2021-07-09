78e0fd7a3240f5a5df936986385186c2c8b6c01a
Thibaut <thibaut.dubois@datastorm.fr>
2021-07-09 11:53:40 +0200

    Add controls to chec_loss to ensure that the provided loss is vectorial.

--- R/check_loss.R
+++ R/check_loss.R
@@ -13,7 +13,11 @@
 #'      \item{function}{ If loss.type is a function, the derivative should be provided to be used (it is not automatically 
 #'      computed).}
 #' }
+#' @param Y \code{numeric} (NULL). (Optional) Target values (to perform some checks).
+#' @param model \code{character} (NULL). (Optional) Model used (to perform some checks).
 #' @param use_cpp \code{boolean}. Whether or not to use cpp function to increase perf.
+#' 
+#' @importFrom methods formalArgs
 #' 
 #' @return loss.type
 #'
@@ -42,11 +46,11 @@ check_loss <- function(loss.type,
     if (! is.null(Y) && min(Y) <= 0 && loss.type$name == "percentage") {
       stop("Y should be non-negative for percentage loss function.")
     }
-    if (loss.type$name != "square" && model == "Ridge") {
+    if (loss.type$name != "square" && ! is.null(model) && model == "Ridge") {
       stop(paste("Square loss is require for Ridge model."))
     }
-    
-  } else {
+  } 
+  else {
     if (use_cpp == TRUE && class(loss.type) == "function") {
       stop("Custom loss functions are not yet available when use_cpp == TRUE.") 
     }
@@ -66,6 +70,19 @@ check_loss <- function(loss.type,
       } 
     } else if (! is.logical(loss.gradient)) {
       stop("when loss.type is a function, loss.gradient should either be a function or set to FALSE.")
+    }
+    # check if provided loss is vectorial
+    is_vect <- tryCatch({
+      ctrl_1 <- loss.type(1, 2)
+      ctrl_2 <- loss.type(3.14, 2)
+      ctrl_3 <- loss.type(c(1, 3.14), 2)
+      all(ctrl_3 == c(ctrl_1, ctrl_2))
+    }, error = function(e) {
+      stop("Error when calling provided loss : \n", 
+           e$message)
+    })
+    if (! all(is_vect)) {
+      stop("The provided loss must be vectorial.")
     }
   }
   
