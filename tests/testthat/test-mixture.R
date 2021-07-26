082c9ef7448519056bf44ad663a1201cbf70e5ec
Benoit Thieurmel <benoit.thieurmel@datastorm.fr>
2021-07-26 17:26:36 +0200

    predict : add without awake test + R vs cpp

--- tests/testthat/test-mixture.R
+++ tests/testthat/test-mixture.R
@@ -258,7 +258,7 @@ test_that("Quantile mixture are ok", {
 })
 
 # test of predict function
-test_that("Predict method is ok", {
+test_that("Predict method is ok, with and without awake, use_cpp or not", {
   for (model in c("BOA", "MLpol", "MLprod", "MLewa", "FS", "Ridge")) {
     for (possible_loss in c("percentage", "absolute", "square", "pinball")) {
       cur_loss <- list("name" = possible_loss)
@@ -270,20 +270,59 @@ test_that("Predict method is ok", {
       m <- mixture(model = model, loss.type = cur_loss, quiet = TRUE)
       expect_warning(predict(m, quiet = TRUE))
       
+      # with awake
       # single online prediction and sequential prediction return similar models
-      m1 <- predict(m, newY = Y, newexperts = X, type = "m", awake = awake, quiet = TRUE)
-      m2 <- m
+      m1_cpp <- predict(m, newY = Y, newexperts = X, type = "m", awake = awake, quiet = TRUE, use_cpp = TRUE)
+      m2_cpp <- m
       for (t in 1:n) {
-        m2 <- predict(m2, newY = Y[t], newexperts = X[t, ], online = TRUE, type = "model",
-                      awake = awake[t, ], quiet = TRUE)
+        m2_cpp <- predict(m2_cpp, newY = Y[t], newexperts = X[t, ], online = TRUE, type = "model",
+                      awake = awake[t, ], quiet = TRUE, use_cpp = TRUE)
       }
-      expect_equal(m1, m2)
+      expect_equal(m1_cpp, m2_cpp)
+      
+      m1_r <- predict(m, newY = Y, newexperts = X, type = "m", awake = awake, quiet = TRUE, use_cpp = FALSE)
+      m2_r <- m
+      for (t in 1:n) {
+        m2_r <- predict(m2_r, newY = Y[t], newexperts = X[t, ], online = TRUE, type = "model",
+                      awake = awake[t, ], quiet = TRUE, use_cpp = FALSE)
+      }
+      expect_equal(m1_r, m2_r)
+      
+      expect_equal(m1_r, m1_cpp)
       
       # batch prediction is ok
-      m1 <- predict(m, newY = Y, newexperts = X, type = "m", online = FALSE, awake = awake, quiet = TRUE)
-      expect_equal(m1$coefficients, m2$coefficients)
+      m1 <- predict(m, newY = Y, newexperts = X, type = "m", online = FALSE, awake = awake, quiet = TRUE, use_cpp = TRUE)
+      expect_equal(m1$coefficients, m2_cpp$coefficients)
       
-      expect_warning(m2 <- predict(m, newexperts = X, type = "r", online = FALSE, awake = awake, quiet = TRUE))
+      expect_warning(m2 <- predict(m, newexperts = X, type = "r", online = FALSE, awake = awake, quiet = TRUE, use_cpp = TRUE))
+      expect_equal(m1$prediction, m2)
+      
+      
+      # without awake
+      # single online prediction and sequential prediction return similar models
+      m1_cpp <- predict(m, newY = Y, newexperts = X, type = "m", awake = NULL, quiet = TRUE, use_cpp = TRUE)
+      m2_cpp <- m
+      for (t in 1:n) {
+        m2_cpp <- predict(m2_cpp, newY = Y[t], newexperts = X[t, ], online = TRUE, type = "model",
+                          awake = NULL, quiet = TRUE, use_cpp = TRUE)
+      }
+      expect_equal(m1_cpp, m2_cpp)
+      
+      m1_r <- predict(m, newY = Y, newexperts = X, type = "m", awake = NULL, quiet = TRUE, use_cpp = FALSE)
+      m2_r <- m
+      for (t in 1:n) {
+        m2_r <- predict(m2_r, newY = Y[t], newexperts = X[t, ], online = TRUE, type = "model",
+                        awake = NULL, quiet = TRUE, use_cpp = FALSE)
+      }
+      expect_equal(m1_r, m2_r)
+      
+      expect_equal(m1_r, m1_cpp)
+      
+      # batch prediction is ok
+      m1 <- predict(m, newY = Y, newexperts = X, type = "m", online = FALSE, awake = NULL, quiet = TRUE, use_cpp = TRUE)
+      expect_equal(m1$coefficients, m2_cpp$coefficients)
+      
+      expect_warning(m2 <- predict(m, newexperts = X, type = "r", online = FALSE, awake = NULL, quiet = TRUE, use_cpp = TRUE))
       expect_equal(m1$prediction, m2)
     }
   }
